1dc28cf9da33e9c368184287cac127b53ec691e5
Denton Poss <dentonposs@gmail.com>
2014-11-16 07:41:21 -0600

    First Commit

---
+++ SteamBot/API.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace SteamBot
+{
+    class API
+    {
+
+        public static Dictionary<long, int> points;
+
+        //Magical function to make multiples of 1/9 out of any decimal number
+        //This is especially useful for turning the raw calculated prices into actual prices
+        public static float ScrapifyPrice(float price)
+        {
+            float end = price - (float)Math.Truncate(price);
+            end *= 10;
+            end = (float)Math.Truncate(end);
+            end *= 11;
+            if (end == 99)
+                end++;
+            end /= 100;
+            float final = (float)Math.Truncate(price) + end;
+            return final;
+        }
+
+        //This method fetches the amount of Keys and Refined Metal from any given Steam inventory
+        //As a rule, I try to keep all downloads into strings in order to reduce filesystem footprint. However, this causes a bit more RAM usage.
+        //TODO Cache API Results
+        public static KeyValuePair<int, int> GetCurrencyStock(UInt64 ID)
+        {
+            var items = new List<KeyValuePair<int, int>>();
+            //The key is the amount of keys, and the value is the amount of refined metal
+            //In case of an error, the key is -1, and the value is the status code from the Steam API
+            //If the Steam API completely failed, then both the key and value are -1
+            try
+            {
+                WebClient client = new WebClient();
+                String backpackFile = client.DownloadString("http://api.steampowered.com/IEconItems_440/GetPlayerItems/v0001/?key=C82BCA5DD2DFE549BE112B33510C0276&steamid=" + ID);
+                dynamic result = Newtonsoft.Json.JsonConvert.DeserializeObject(backpackFile);
+
+                if (result.result.status == 15)
+                {
+                    return new KeyValuePair<int, int>(-1, 15);
+                }
+                else
+                {
+                    foreach (var item in result.result.items)
+                    {
+                        items.Add(new KeyValuePair<int, int>(Convert.ToInt32(item.defindex + ""), Convert.ToInt32(item.quality + "")));
+                    }
+                    int refcount = 0;
+                    int keycount = 0;
+                    foreach (var item in items)
+                    {
+                        if (item.Key == 5002)
+                        {
+                            refcount++;
+                        }
+                        if (item.Key == 5021)
+                        {
+                            keycount++;
+                        }
+                    }
+                    return new KeyValuePair<int, int>(keycount, refcount);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return new KeyValuePair<int, int>(-1, -1);
+        }
+
+        //The loyalty point system works using a dictionary of SteamIDs and integers.
+        //The dictionary is deserialized when the program starts, and serialized everytime points are added and every time there is a graceful shutdown
+        public static void DeserializePoints()
+        {
+            if (!File.Exists("points.json"))
+            {
+                File.WriteAllText("points.json", "{\"76561198039982559\":0}");
+            }
+            string json = File.ReadAllText("points.json");
+            points = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<long, int>>(json);
+            Console.WriteLine();
+        }
+
+        public static void SerializePoints()
+        {
+            string serial = Newtonsoft.Json.JsonConvert.SerializeObject(points);
+            File.WriteAllText("points.json", serial);
+        }
+
+        public static void AddPoints(long ID, int point)
+        {
+            int current = GetPoints(ID);
+            points[ID] = current + point;
+            SerializePoints();
+        }
+
+        public static int GetPoints(long ID)
+        {
+            if (points.ContainsKey(ID))
+                return points[ID];
+            else
+            {
+                points.Add(ID, 0);
+                return 0;
+            }
+        }
+
+        //This method gets the price for an item off of backpack.tf, given a defindex.
+        //As of now, it only works for Craftable, Tradable, and the first quality of the given item.
+        //The method was made primarily with keys in mind
+        //TODO Cache API result
+        public static double GetPrices(string defindex)
+        {
+            WebClient client = new WebClient();
+            string priceFile = client.DownloadString("http://backpack.tf/api/IGetPrices/v4/?key=514f66754bd7b8325a00000d");
+            dynamic result = Newtonsoft.Json.JsonConvert.DeserializeObject(priceFile);
+            double keyprice = 0;
+            //Big nasty JSON parse function. Cmon, Brad, fix your API. Perhaps a feature to fetch data for a defindex via the URL?
+            try
+            {
+                foreach (var items in result.response.items)
+                {
+                    foreach (var item in items)
+                    {
+                        string indexstr = item.defindex.ToString();
+                        if (indexstr.Contains(defindex))
+                        {
+                            foreach (var price in item.prices)
+                            {
+                                foreach (var quality in price)
+                                {
+                                    Console.WriteLine("Key price fetched: {0} {1}", quality.Tradable.Craftable[0].value, quality.Tradable.Craftable[0].currency);
+                                    keyprice = Convert.ToDouble(quality.Tradable.Craftable[0].value.ToString());
+                                    return keyprice;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Parse complete");
+            return -1;
+        }
+    }
+}
---
+++ SteamBot/Program.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Timers;
+using System.Linq;
+using System.Media;
+using System.Net;
+using SteamKit2;
+
+namespace SteamBot
+{
+    class MainClass
+    {
+        static SteamClient client;
+        static CallbackManager manager;
+        static SteamUser steamuser;
+        static SteamFriends friends;
+        static SteamTrading trade;
+        static bool isRunning;
+        static string user, pass;
+        static string code, auth;
+        static bool newtext;
+        static string newmsg;
+
+        static string name = "ʢ"; //Steam persona name of the bot
+
+        static float sellmult = .05F; //Amount to increase price based off of backpack.tf price when selling
+        static float buymult = .04F; //Amount to decrease price basd off of backpack.tf price when buying
+
+        static UInt64 ownerID = 76561198039982559; //Steam ID of the bot admin
+
+        public static void Main(string[] args)
+        {
+            API.DeserializePoints();
+            if (!File.Exists("login.json"))
+            {
+                Console.WriteLine("Your login info needs to be cached.");
+                Console.Write("Username? ");
+                user = Console.ReadLine();
+                Console.Write("Password? ");
+                pass = Console.ReadLine();
+                File.WriteAllText("login.json", Newtonsoft.Json.JsonConvert.SerializeObject(new KeyValuePair<string, string>(user, pass)));
+            }
+            var login = Newtonsoft.Json.JsonConvert.DeserializeObject<KeyValuePair<string, string>>(File.ReadAllText("login.json"));
+            user = login.Key;
+            pass = login.Value;
+            client = new SteamClient();
+            manager = new CallbackManager(client);
+            steamuser = client.GetHandler<SteamUser>();
+            friends = client.GetHandler<SteamFriends>();
+            trade = client.GetHandler<SteamTrading>();
+            InitCallbacks();
+            Console.WriteLine("Connecting to Steam...");
+            client.Connect();
+            Thread thread = new Thread(new ThreadStart(ReadLine));
+            thread.Start();
+            isRunning = true;
+            while (isRunning)
+            {
+                manager.RunCallbacks();
+                if (newtext)
+                {
+                    string[] cmds = newmsg.Split();
+                    if (cmds[0] == "shutdown")
+                    {
+                        API.SerializePoints();
+                        Environment.Exit(0);
+                    }
+                    newtext = false;
+                }
+            }
+        }
+
+        public static void InitCallbacks()
+        {
+            //SteamClient
+            new Callback<SteamClient.ConnectedCallback>(OnConnect, manager);
+            new Callback<SteamClient.DisconnectedCallback>(OnDisconnect, manager);
+            //SteamUser
+            new Callback<SteamUser.LoggedOnCallback>(OnLogin, manager);
+            new Callback<SteamUser.LoggedOffCallback>(OnLogout, manager);
+            new Callback<SteamUser.UpdateMachineAuthCallback>(OnAuth, manager);
+            new Callback<SteamUser.AccountInfoCallback>(OnAccountInfo, manager);
+            //SteamFriends
+            new Callback<SteamFriends.FriendsListCallback>(OnFriendsList, manager);
+            new Callback<SteamFriends.PersonaStateCallback>(OnPersonaState, manager);
+            new Callback<SteamFriends.FriendAddedCallback>(OnFriendAdded, manager);
+            new Callback<SteamFriends.FriendMsgCallback>(OnMsg, manager);
+        }
+
+        static void OnConnect(SteamClient.ConnectedCallback callback)
+        {
+            if (callback.Result != EResult.OK)
+            {
+                Console.WriteLine("Steam connection failed: {0}.", callback.Result);
+                isRunning = false;
+                return;
+            }
+
+            Console.WriteLine("Connected to Steam. Logging in as {0}.", user);
+            byte[] sentryhash = null;
+            if (File.Exists("sentry.bin"))
+            {
+                byte[] sentryfile = File.ReadAllBytes("sentry.bin");
+                sentryhash = CryptoHelper.SHAHash(sentryfile);
+            }
+            steamuser.LogOn(new SteamUser.LogOnDetails
+            {
+                Username = user,
+                Password = pass,
+                AuthCode = auth,
+                TwoFactorCode = code,
+                SentryFileHash = sentryhash,
+            });
+        }
+
+        static void OnDisconnect(SteamClient.DisconnectedCallback callback)
+        {
+            Console.WriteLine("Reconnecting to Steam...");
+            client.Connect();
+        }
+
+        static void OnLogin(SteamUser.LoggedOnCallback callback)
+        {
+            bool isSteamGuard = callback.Result == EResult.AccountLogonDenied;
+            bool is2FA = callback.Result == EResult.AccountLogonDeniedNeedTwoFactorCode;
+
+            if (isSteamGuard || is2FA)
+            {
+                Console.WriteLine("This account is SteamGuard protected!");
+
+                if (is2FA)
+                {
+                    Console.Write("Please enter your 2 factor auth code from your authenticator app: ");
+                    code = Console.ReadLine();
+                }
+                else
+                {
+                    Console.Write("Please enter the auth code sent to the email at {0}: ", callback.EmailDomain);
+                    auth = Console.ReadLine();
+                }
+
+                return;
+            }
+
+            if (callback.Result != EResult.OK)
+            {
+                Console.WriteLine("Unable to logon to Steam: {0} / {1}", callback.Result, callback.ExtendedResult);
+
+                isRunning = false;
+                return;
+            }
+
+            Console.WriteLine("Successfully logged on!");
+        }
+
+        static void OnLogout(SteamUser.LoggedOffCallback callback)
+        {
+            Console.WriteLine("Logged out of Steam. {0}", callback.Result);
+        }
+
+        static void OnAuth(SteamUser.UpdateMachineAuthCallback callback)
+        {
+            Console.WriteLine("Updating sentryfile...");
+            byte[] sentryHash = CryptoHelper.SHAHash(callback.Data);
+            File.WriteAllBytes("sentry.bin", callback.Data);
+            steamuser.SendMachineAuthResponse(new SteamUser.MachineAuthDetails
+            {
+                JobID = callback.JobID,
+
+                FileName = callback.FileName,
+
+                BytesWritten = callback.BytesToWrite,
+                FileSize = callback.Data.Length,
+                Offset = callback.Offset,
+
+                Result = EResult.OK,
+                LastError = 0,
+
+                OneTimePassword = callback.OneTimePassword,
+
+                SentryFileHash = sentryHash,
+            });
+
+            Console.WriteLine("Done!");
+        }
+
+        static void OnAccountInfo(SteamUser.AccountInfoCallback callback)
+        {
+            friends.SetPersonaState(EPersonaState.Online);
+        }
+
+        static void OnFriendsList(SteamFriends.FriendsListCallback callback)
+        {
+            friends.SetPersonaName(name);
+            foreach (var friend in callback.FriendList)
+            {
+                if (friend.Relationship == EFriendRelationship.RequestRecipient)
+                {
+                    // this user has added us, let's add him back
+                    friends.AddFriend(friend.SteamID);
+                }
+            }
+        }
+
+        static void OnPersonaState(SteamFriends.PersonaStateCallback callback)
+        {
+
+        }
+
+        static void OnFriendAdded(SteamFriends.FriendAddedCallback callback)
+        {
+            //Console.WriteLine("{0} is now a friend", callback.PersonaName);
+            //client.Disconnect();
+        }
+
+        static void OnMsg(SteamFriends.FriendMsgCallback callback)
+        {
+            string msg = callback.Message;
+            if (msg == "stock")
+            {
+                friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, "Geting Steam backpack data...");
+                var stock = API.GetCurrencyStock(steamuser.SteamID);
+                friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, String.Format("Keys: {0}, Refined Metal: {1}", stock.Key, stock.Value));
+            }
+            if (msg == "price")
+            {
+                friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, "Getting price data from backpack.tf...");
+                float baseprice = (float)API.GetPrices("5021");
+                float sell = baseprice + (baseprice * sellmult);
+                float buy = baseprice - (baseprice * buymult);
+                sell = API.ScrapifyPrice(sell);
+                buy = API.ScrapifyPrice(buy);
+                friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, String.Format("Selling keys for: {0} refined\nBuying keys for: {1} refined", sell, buy));
+            }
+            if (msg == "keyval")
+            {
+                friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, "Geting Steam backpack data...");
+                var stock = API.GetCurrencyStock(callback.Sender);
+                int keys = stock.Key;
+                friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, "Getting price data from backpack.tf...");
+                float baseprice = (float)API.GetPrices("5021");
+                float buy = baseprice - (baseprice * buymult);
+                buy = API.ScrapifyPrice(buy);
+                float totalPay = keys * buy;
+                switch (keys)
+                {
+                    case -1:
+                        if(stock.Value == 15)
+                            friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, "Your Steam inventory is private. Please make sure it is public in order to trade.");
+                        else
+                            friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, "There was an error getting your inventory. Please try again.");
+                        break;
+                    case 0:
+                        friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, "You have no keys.");
+                        break;
+                    case 1:
+                        friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, String.Format("I can buy your key for {0} refined.", totalPay));
+                        break;
+                    case 2:
+                        friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, String.Format("I can buy both of your keys for {0} refined.", totalPay));
+                        break;
+                    default:
+                        friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, String.Format("I can buy all {0} of your keys for {1} refined.", keys, totalPay));
+                        break;
+                }
+            }
+            if (msg == "points")
+            {
+                int points = API.GetPoints((long) callback.Sender.ConvertToUInt64());
+                friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, String.Format("You have {0} loyalty points.", points));
+            }
+            if (msg == "help")
+            {
+                friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, File.ReadAllText("help.txt"));
+            }
+            if (callback.Sender.ConvertToUInt64() == ownerID)
+            {
+                string[] cmds = msg.Split();
+                if(cmds[0] == "sellmult" && cmds.Length > 1){
+                    try
+                    {
+                        sellmult = (float)Convert.ToDouble(cmds[1]);
+                        float baseprice = (float)API.GetPrices("5021");
+                        float sell = baseprice + (baseprice * sellmult);
+                        sell = API.ScrapifyPrice(sell);
+                        friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, String.Format("Selling multiplier changed to {0}. Now selling keys for {1} refined", Convert.ToDouble(cmds[1]), sell));
+                    }
+                    catch (Exception e)
+                    {
+                        friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, "Bad formatting!");
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                if (cmds[0] == "buymult" && cmds.Length > 1)
+                {
+                    try
+                    {
+                        buymult = (float)Convert.ToDouble(cmds[1]);
+                        float baseprice = (float)API.GetPrices("5021");
+                        float buy = baseprice - (baseprice * buymult);
+                        buy = API.ScrapifyPrice(buy);
+                        friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, String.Format("Buying multiplier changed to {0}. Now Buying keys for {1} refined", Convert.ToDouble(cmds[1]), buy));
+                    }
+                    catch (Exception e)
+                    {
+                        friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, "Bad formatting!");
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                if (cmds[0] == "profit")
+                {
+                    friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, "Getting price data from backpack.tf...");
+                    float baseprice = (float)API.GetPrices("5021");
+                    float sell = baseprice + (baseprice * sellmult);
+                    float buy = baseprice - (baseprice * buymult);
+                    sell = API.ScrapifyPrice(sell);
+                    buy = API.ScrapifyPrice(buy);
+                    friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, String.Format("We are making {0} refined off of every sold key, {1} refined off of every purchased key, and {2} refined off of every key bought and sold.", API.ScrapifyPrice(sell - baseprice), API.ScrapifyPrice(baseprice - buy), API.ScrapifyPrice((sell - baseprice) + (baseprice - buy))));
+                }
+                if (cmds[0] == "shutdown")
+                {
+                    API.SerializePoints();
+                    Environment.Exit(0);
+                }
+                if (cmds[0] == "addpoint")
+                {
+                    try
+                    {
+                        API.AddPoints((long)Convert.ToUInt64(cmds[1]), Convert.ToInt32(cmds[2]));
+                    }
+                    catch (Exception e)
+                    {
+                        friends.SendChatMessage(callback.Sender, EChatEntryType.ChatMsg, "Bad formatting!");
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+        }
+
+        public static void ReadLine()
+        {
+            while (true)
+            {
+                newmsg = Console.ReadLine();
+                newtext = true;
+            }
+        }
+    }
+}
